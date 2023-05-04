bebc9a44cce10a8bb39c41a0c311a61413c6e7c3
Oscar <oscargomezsedas@gmail.com>
2023-05-04 18:51:46 +0200

    readtext utility created

---
+++ src/Utilities/ReadText.java
@@ -0,0 +1,11 @@
+package Utilities;
+
+import java.util.Scanner;
+
+public class ReadText {
+    Scanner scanner = new Scanner(System.in);
+
+    public String readLine(){
+        return scanner.nextLine();
+    }
+}
